b6700cc38095ed4998912e731e680156a8ca8421
Brent Vatne <brentvatne@gmail.com>
2017-10-10 10:15:28 -0700

    Add jitpack.io to build.gradle to maybe fix Android build

--- android/app/src/main/java/io/expo/buddybuildexample/generated/ExponentBuildConstants.java
+++ android/app/src/main/java/io/expo/buddybuildexample/generated/ExponentBuildConstants.java
@@ -3,6 +3,6 @@ package io.expo.buddybuildexample.generated;
 // This file is auto-generated. Please don't rename!
 public class ExponentBuildConstants {
 
-  public static final String DEVELOPMENT_URL = "";
+  public static final String DEVELOPMENT_URL = "expc9858f22aaeb4551a750d978e87916e1://7s-pxs.notbrent.testexpokitbuddybuild.exp.direct:80";
 
 }
